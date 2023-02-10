55ca7d73b6443af69a633d19ddb81c35c0482c62
DESKTOP-67VUI61\user <qufehdwls@naver.com>
2023-02-10 16:51:51 +0900

    day13

---
+++ frontend_work/front_day03/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+// CSS는 App.css 파일을 만들어 스타일 적용 후 App.js에서 import하면 된다.
+
+function Menu(props) {
+    const [menuItems, setMenuItems] = useState(["HOME", "PROFILE", "GALLERY", "LECTURE", "GUEST"]);
+
+    return (
+        <>
+            <ul>
+                {menuItems.map((item, i) => {
+                    return <li key="i">item</li>
+                })}
+            </ul>
+        </>
+    )
+}
+
+function App() {
+    return (<>
+        <h1>Comstudy SW School</h1>
+        <Menu></Menu>
+    </>)
+}
+
+export default App;
