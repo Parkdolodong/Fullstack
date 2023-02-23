9d9c234a344ddf5f0d83599f1ce1fd1813474bc5
DESKTOP-67VUI61\user <qufehdwls@naver.com>
2023-02-23 10:00:14 +0900

    Day 21

---
+++ backend_work01/backend_day06/test_module.js
@@ -0,0 +1,16 @@
+const partModule = {
+    value : "korea",
+    func : function() {
+        console.log("value = ", this.value);
+    }
+}
+
+module.exports.moduleName = "partModule";
+module.exports.partModule = partModule;
+
+// module.exports = {
+//     value : "korea",
+//     func : function() {
+//         console.log("value = ", this.value);
+//     }
+// }
