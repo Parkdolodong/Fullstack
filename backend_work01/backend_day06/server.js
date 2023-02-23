068d69a4b6223275e383d8f4314a5f772d261713
DESKTOP-67VUI61\user <qufehdwls@naver.com>
2023-02-23 13:25:48 +0900

    Day 21

--- backend_work01/backend_day06/server.js
+++ backend_work01/backend_day06/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const router = express.Router();
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 
@@ -12,15 +12,17 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.use(cookieParser());
-app.use(session({
-    secret:'my key',
-    resave:true,
-    saveUninitialized:true
-}));
+app.use(
+  session({
+    secret: "my key",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 const uri = "mongodb://127.0.0.1";
 const client = new MongoClient(uri, { useUnifiedTopology: true });
@@ -34,6 +36,8 @@ async function connectDB() {
     db = await client.db("vehicle");
     localDB = await client.db("local");
     console.log("Connected successfully to server");
+    app.set("db", db);
+    app.set("localDB", localDB);
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -41,11 +45,13 @@ async function connectDB() {
 }
 
 ///////--------------------------------
-app.set("db", db);
-app.set("localDB", localDB);
+const routerModule = require("./router_module");
+routerModule(router, app);
 
-server.listen(app.get("port"), ()=>{
-    console.log("http://localhost:"+ app.get("port"));
-    console.log("Node.js 서버 실행 중 ...");
-    connectDB().catch(console.dir);
+app.use("/", router);
+
+server.listen(app.get("port"), () => {
+  console.log("http://localhost:" + app.get("port"));
+  console.log("Node.js 서버 실행 중 ...");
+  connectDB().catch(console.dir);
 });
